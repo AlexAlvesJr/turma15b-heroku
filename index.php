2b9d58ab925555dd2ab2a750a5307b698cfe1ed9
AlexAlvesJr <alexalves_jr@hotmail.com>
2021-03-08 15:22:20 -0300

    adc imagem hero

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
 
         <div class="row">
           <div class="col-lg-4" data-aos="fade-right">
-            <img src="/assets/img/profile-alex.jpg" class="img-fluid" alt="">
+            <img src="/assets/img/hero-alex.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
             <h3>Engenheiro Mecânico &amp; Analista de TI.</h3>
